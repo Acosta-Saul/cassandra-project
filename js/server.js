0a2428a4451462df301f0633aebf9b3023ca1eec
Luis Santacruz <luisantacruzkids@gmail.com>
2024-01-07 23:34:23 -0400

    Fix: conguracion de server.js

--- js/server.js
+++ js/server.js
@@ -27,6 +27,21 @@ app.get('/', async (req, res) => {
 app.delete('/delete/:id', async (req, res) => {
   const userId = req.params.id;
 
+  try {
+    const result = await client.execute('DELETE FROM usuarios WHERE id = ?', [userId]);
+
+    // Verifica si la eliminación fue exitosa
+    if (result.wasApplied()) {
+      res.json({ message: `Usuario con ID ${userId} eliminado correctamente.` });
+    } else {
+      res.status(404).json({ error: `Usuario con ID ${userId} no encontrado.` });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error al eliminar usuario de Cassandra.' });
+  }
+});
+
 // Endpoint para insertar un usuario
 app.post('/usuarios', async (req, res) => {
   const { nombre, apellido } = req.body;
@@ -45,9 +60,6 @@ app.post('/usuarios', async (req, res) => {
   }
 });
 
-
-
-
 // Endpoint para manejar las actualizaciones de los usuarios
 app.patch('/update/:id', async (req, res) => {
   const userId = req.params.id;
@@ -65,21 +77,6 @@ app.patch('/update/:id', async (req, res) => {
 });
 
 
-
-  try {
-    const result = await client.execute('DELETE FROM usuarios WHERE id = ?', [userId]);
-
-    // Verifica si la eliminación fue exitosa
-    if (result.wasApplied()) {
-      res.json({ message: `Usuario con ID ${userId} eliminado correctamente.` });
-    } else {
-      res.status(404).json({ error: `Usuario con ID ${userId} no encontrado.` });
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Error al eliminar usuario de Cassandra.' });
-  }
-});
 
 // Inicia el servidor
 app.listen(port, () => {
