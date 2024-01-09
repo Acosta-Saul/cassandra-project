a52adc4e3c96dc2810bf733c73ccd62fe8a52e27
Acosta-Saul <145149375+Acosta-Saul@users.noreply.github.com>
2024-01-09 16:45:42 -0400

    create: nueva rama boton-filtro

--- js/filter.js
+++ js/filter.js
@@ -4,7 +4,12 @@ function mostrarDatos(){
   
   console.log(seleccion);
 
+  const table = document.createElement('table');
+  const thead = document.createElement('thead');
+  const tbody = document.createElement('tbody');
 
+    // Creo un array con los campos que quiero colocar en la tabla que voy a crear y los datos que quiero mostrar en dicha tabla
+    const campos = ['nombre', 'marca', 'unidades', 'promocion', 'rango_etario', 'genero', 'creditos', 'fecha_vencimiento'];
 
 
 
